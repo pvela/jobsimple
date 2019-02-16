eb1190ad31329e0291109b80296f054dd82aea45
Steve <astv99@outlook.com>
2019-02-16 12:27:06 -0700

    Created Header component; moved Log out button to Header

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,149 +2,7 @@
   <!-- Wrapper -->
   <div id="wrapper">
 
-  <!-- Header Container
-  ================================================== -->
-  <header id="header-container" class="fullwidth">
-
-    <!-- Header -->
-    <div id="header">
-      <div class="container">
-        
-        <!-- Left Side Content -->
-        <div class="left-side">
-          
-          <!-- Logo -->
-          <div id="logo">
-            <a href="index.html"><img src="@/assets/images/logo.png" alt="Hireo"></a>
-          </div>
-
-          <!-- Main Navigation -->
-          <nav id="navigation">
-            <ul id="responsive">
-
-              <li><a href="#" class="current">Home</a>
-                <ul class="dropdown-nav">
-                  <li><a href="index.html">Home 1</a></li>
-                  <li><a href="index-2.html">Home 2</a></li>
-                  <li><a href="index-3.html">Home 3</a></li>
-                </ul>
-              </li>
-
-              <li><a href="#">Find Work</a>
-                <ul class="dropdown-nav">
-                  <li><a href="#">Browse Jobs</a>
-                    <ul class="dropdown-nav">
-                      <li><a href="jobs-list-layout-full-page-map.html">Full Page List + Map</a></li>
-                      <li><a href="jobs-grid-layout-full-page-map.html">Full Page Grid + Map</a></li>
-                      <li><a href="jobs-grid-layout-full-page.html">Full Page Grid</a></li>
-                      <li><a href="jobs-list-layout-1.html">List Layout 1</a></li>
-                      <li><a href="jobs-list-layout-2.html">List Layout 2</a></li>
-                      <li><a href="jobs-grid-layout.html">Grid Layout</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="#">Browse Tasks</a>
-                    <ul class="dropdown-nav">
-                      <li><a href="tasks-list-layout-1.html">List Layout 1</a></li>
-                      <li><a href="tasks-list-layout-2.html">List Layout 2</a></li>
-                      <li><a href="tasks-grid-layout.html">Grid Layout</a></li>
-                      <li><a href="tasks-grid-layout-full-page.html">Full Page Grid</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="browse-companies.html">Browse Companies</a></li>
-                  <li><a href="single-job-page.html">Job Page</a></li>
-                  <li><a href="single-task-page.html">Task Page</a></li>
-                  <li><a href="single-company-profile.html">Company Profile</a></li>
-                </ul>
-              </li>
-
-              <li><a href="#">For Sponsors</a>
-                <ul class="dropdown-nav">
-                  <li><a href="#">Find a Freelancer</a>
-                    <ul class="dropdown-nav">
-                      <li><a href="freelancers-grid-layout-full-page.html">Full Page Grid</a></li>
-                      <li><a href="freelancers-grid-layout.html">Grid Layout</a></li>
-                      <li><a href="freelancers-list-layout-1.html">List Layout 1</a></li>
-                      <li><a href="freelancers-list-layout-2.html">List Layout 2</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="single-freelancer-profile.html">Freelancer Profile</a></li>
-                  <li><a href="dashboard-post-a-job.html">Post a Job</a></li>
-                  <li><a href="dashboard-post-a-task.html">Post a Task</a></li>
-                </ul>
-              </li>
-
-              <li><a href="#">Dashboard</a>
-                <ul class="dropdown-nav">
-                  <li><a href="dashboard.html">Dashboard</a></li>
-                  <li><a href="dashboard-messages.html">Messages</a></li>
-                  <li><a href="dashboard-bookmarks.html">Bookmarks</a></li>
-                  <li><a href="dashboard-reviews.html">Reviews</a></li>
-                  <li><a href="dashboard-manage-jobs.html">Jobs</a>
-                    <ul class="dropdown-nav">
-                      <li><a href="dashboard-manage-jobs.html">Manage Jobs</a></li>
-                      <li><a href="dashboard-manage-candidates.html">Manage Candidates</a></li>
-                      <li><a href="dashboard-post-a-job.html">Post a Job</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="dashboard-manage-tasks.html">Tasks</a>
-                    <ul class="dropdown-nav">
-                      <li><a href="dashboard-manage-tasks.html">Manage Tasks</a></li>
-                      <li><a href="dashboard-manage-bidders.html">Manage Bidders</a></li>
-                      <li><a href="dashboard-my-active-bids.html">My Active Bids</a></li>
-                      <li><a href="dashboard-post-a-task.html">Post a Task</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="dashboard-settings.html">Settings</a></li>
-                </ul>
-              </li>
-
-              <li><a href="#">Pages</a>
-                <ul class="dropdown-nav">
-                  <li><a href="pages-blog.html">Blog</a></li>
-                  <li><a href="pages-pricing-plans.html">Pricing Plans</a></li>
-                  <li><a href="pages-checkout-page.html">Checkout Page</a></li>
-                  <li><a href="pages-invoice-template.html">Invoice Template</a></li>
-                  <li><a href="pages-user-interface-elements.html">User Interface Elements</a></li>
-                  <li><a href="pages-icons-cheatsheet.html">Icons Cheatsheet</a></li>
-                  <li><a href="pages-login.html">Login & Register</a></li>
-                  <li><a href="pages-404.html">404 Page</a></li>
-                  <li><a href="pages-contact.html">Contact</a></li>
-                </ul>
-              </li>
-
-            </ul>
-          </nav>
-          <div class="clearfix"></div>
-          <!-- Main Navigation / End -->
-          
-        </div>
-        <!-- Left Side Content / End -->
-
-
-        <!-- Right Side Content / End -->
-        <div class="right-side">
-
-          <!-- Mobile Navigation Button -->
-          <span class="mmenu-trigger">
-            <button class="hamburger hamburger--collapse" type="button">
-              <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-              </span>
-            </button>
-          </span>
-
-        </div>
-        <!-- Right Side Content / End -->
-
-      </div>
-    </div>
-    <!-- Header / End -->
-    <button class="button ripple-effect" @click="logout">Log out</button>
-  </header>
-  <div class="clearfix"></div>
-  <!-- Header Container / End -->
-
-
+  <Header></Header>
 
   <!-- Intro Banner
   ================================================== -->
@@ -1021,6 +879,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import { store } from '../store';
 import Fortmatic from 'fortmatic';
+import Header from './Header.vue';
 const fm = new Fortmatic('pk_test_80863C4FA1028899');
 
 
@@ -1030,6 +889,9 @@ export default {
     return {
       store
     };
+  },
+  components: {
+    Header
   },
   methods: {
     logout() {
